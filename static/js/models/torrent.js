737a4ed8b8167d802d7d927937c0edb0a15f4ad1
Alex Wright <alex@xeentech.com>
2013-07-28 19:00:09 +0100

    Adding 'getMagnetURI' method to the Torrent model.

--- static/js/models/torrent.js
+++ static/js/models/torrent.js
@@ -2,6 +2,13 @@ define("models/torrent",
     ["backbone"],
     function (Backbone) {
         var Torrent = Backbone.Model.extend({
+            getMagnetURI: function () {
+                return [
+                    "magnet:?xt=urn:",
+                    "btih:", this.get("infoHash"),
+                    "&dn=", this.get("title").replace(/\s+/g, "_")
+                ].join("");
+            }
         });
         return Torrent;
     })
