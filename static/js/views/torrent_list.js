c569df508d28ede99e6bf1f952a1381ed3bde74e
Alex Wright <alex@xeentech.com>
2013-07-28 19:22:22 +0100

    Splitting TorrentListView into list + item views.

--- static/js/views/torrent_list.js
+++ static/js/views/torrent_list.js
@@ -1,8 +1,17 @@
 define("views/torrent_list",
     ["jquery", "underscore", "backbone", "collections/torrents"],
     function ($, _, Backbone, Torrents) {
+        var TorrentListViewItem = Backbone.View.extend({
+            tagName: "li",
+            initialize: function () {
+            },
+            render: function () {
+                this.$el.html("<a href='#'>" + this.model.get("title") + "</a>");
+            }
+        });
         var TorrentListView = Backbone.View.extend({
             initialize: function () {
+                this._views = {};
                 if (!this.collection) {
                     /*  If we're not created with a collection we'll create a
                         fresh 'Torrents' collection. */
@@ -14,12 +23,23 @@ define("views/torrent_list",
                 this.$el.html("<legend>Torrents:</legend><ul></ul>");
             },
             render: function () {
-                var items = this.collection.map(function (m) {
-                    return "<li>" + m.get("title") + "</ul>";
-                });
-                $("ul", this.el).html(items);
+                var items = this.collection.map(this.getListItemView, this);
+                $("ul", this.el).empty().append(items);
 
                 console.log("TorrentListView->render()");
+            },
+            getListItemView: function (model) {
+                if (model.cid in this._views) {
+                    return this._views[model.cid].el;
+                }
+
+                var view = new TorrentListViewItem({
+                    model: model
+                });
+                view.render();
+                this._views[model.cid] = view;
+                console.log("getting view for model: ", model);
+                return view.el;
             }
         });
         return TorrentListView;
